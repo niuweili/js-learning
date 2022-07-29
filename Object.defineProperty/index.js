90b813e6d2c2d2137c5f970d5187805306fbe8ee
niuweili <niuweili@kuaishou.com>
2022-07-29 21:24:41 +0800

    vue2原理初步解析

--- Object.defineProperty/index.js
+++ Object.defineProperty/index.js
@@ -29,25 +29,218 @@ for (const key in obj) {
 }
 
 
-// let person = {}
-// let personName = 'lihua'
+// 2. 自定义getter和setter
+function Person() {
+    let age = 22;
+    let stepList = []
+    const name = 'lihua'
 
-// // 在person上添加namp属性，值为personName
-// Object.defineProperty(person, 'namp', {
-//     get: function () {
-//         console.log('触发了get方法')
-//         return personName
-//     },
-//     set: function (val) {
-//         console.log('触发了set方法')
-//         personName = val
+    Object.defineProperty(this, 'age', {
+        get: function () {
+            console.log('触发了get')
+            return age
+        },
+        set: function (value) {
+            age = value;
+            stepList.push({ val: value })
+        }
+    })
+
+    this.getStepList = function () {
+        return stepList
+    }
+}
+
+const person = new Person();
+console.log('person.age', person.age)
+
+person.age = 33;
+person.age = 44;
+// 输出[ { val: 33 }, { val: 44 } ]
+console.log(person.getStepList())
+
+
+
+// 3.深层嵌套对象无法被监听
+
+// 遇到的问题：get里return obj[key]会执行get方法，引发内存泄漏
+// for (const key in person) {
+//     if (Object.hasOwnProperty.call(person, key)) {
+//         Object.defineProperty(person, key, {
+//             enumerable: true,
+//             configurable: true,
+//             get: function () {
+//                 console.log('get');
+//                 // ！这里会引起内存泄漏，因为每次return person[key]也执行了get
+//                 // return person[key]
+
+//             },
+//             set: function (val) {
+//                 console.log('set-->', val)
+//                 obj[key] = val;
+//             }
+//         })
 //     }
-// })
+// }
 
-// // 访问person.namp，触发了get方法
-// console.log('person.namp:', person.namp)
+// 解决方案：封装一个observer
+function defineProperty(obj, key, val) {
+    Object.defineProperty(obj, key, {
+        enumerable: true,
+        configurable: true,
+        get: function () {
+            console.log('get-->', key);
+            return val
 
-// person.namp = 'luna';
+        },
+        set: function (x) {
+            console.log('set-->', x)
+            val = x;
+        }
+    })
+}
 
-// console.log('person.namp:', person.namp)
+function observer(params) {
+    for (const key in params) {
+        defineProperty(params, key, params[key])
+    }
+}
 
+const person = {
+    name: 'lihua',
+    age: 22,
+    address: {
+        province: "山西",
+        city: "长治"
+    }
+}
+
+observer(person)
+
+// 正常触发set函数
+person.age = 44
+
+// set函数没有被触发（触发了address属性的get），深层对象监听需要递归
+person.address.city = "太原"
+
+// set函数会被触发
+person.address = {
+    province: '河北',
+    city: '承德'
+}
+
+// 新增属性get和set都不会触发
+person.height = 222;
+console.log('person.height-->', person.height)
+
+
+
+// 4.监听数组
+// 深层数组跟对象一样只能监听到get方法
+
+const arr = [1, 2, 3, 4, [5, [6, 7]]];
+
+observer(arr);
+
+// 执行了set方法
+arr[0] = 444;
+
+// 只会执行下标为4的get方法
+arr[4][0] = 555
+
+
+// 5.封装递归函数，深度监听对象/数组的变化
+
+function deepDefineProperty(obj, key, val) {
+    // 如果传入的是一个属性也是一个对象，继续监听
+    if (typeof val === "object" || Array.isArray(val)) {
+        deepObserver(val)
+    }
+    Object.defineProperty(obj, key, {
+        enumerable: true,
+        configurable: true,
+        get: function () {
+            console.log('get-->', key);
+            return val
+
+        },
+        set: function (newVal) {
+            console.log('set-->', newVal)
+            if (typeof newVal === "object") {
+                deepObserver(newVal)
+            }
+            if (val !== newVal) {
+                val = newVal;
+            }
+        }
+    })
+}
+// TODO:为什么要备份一份prototype
+const originProto = Array.prototype
+const arrayProto = Object.create(originProto); // 先copy一份array的原型
+const methodList = [
+    'push',
+    'pop',
+    'shift',
+    'unshift',
+    'splice',
+    'sort',
+    'reverse'
+]
+
+methodList.forEach(method => {
+    arrayProto[method] = function () {
+        originProto[method].apply(this, arguments)
+        console.log('监听赋值', arguments)
+    }
+})
+
+function deepObserver(params) {
+    //如果传入的不是一个对象，return
+    if (typeof params !== "object" || params === null) {
+        return
+    }
+    // 如果是数组，重写原型
+    if (Array.isArray(params)) {
+        params.__proto__ = arrayProto
+        for (const key in params) {
+            // 传入的数组可能是多纬度
+            deepObserver(params[key])
+        }
+    } else {
+        for (const key in params) {
+            deepDefineProperty(params, key, params[key])
+        }
+    }
+}
+
+const deepObj = {
+    a: 1,
+    b: {
+        c: {
+            name: 'a-b-c'
+        }
+    }
+}
+deepObserver(deepObj)
+
+// 执行了set方法并且走了b&c的get方法
+deepObj.b.c.name = 'aaaaaa'
+
+
+const deepArr = [1, 2, 3, 4];
+
+deepObserver(deepArr)
+// 在原型的方法上监听到了改变
+deepArr.push([1111, [22222]])
+
+
+// 6. 定义set方法，实现新增属性也可以监听
+function set(obj, key, val) {
+    deepDefineProperty(obj, key, val)
+}
+
+set(deepArr, [6], 66666)
+
+// 可以执行set方法
+deepArr[6] = 88
--- Object.defineProperty/readme.md
+++ Object.defineProperty/readme.md
@@ -11,19 +11,52 @@ https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects
 #### 1.1 descriptor
 
 
-- configurable： 键值为 true 时，该属性的描述符才能够被改变，同时该属性也能从对应的对象上被删除（默认为false）
+- configurable： configurable 特性表示对象的属性是否可以被删除，以及除 value 和 writable 特性外的其他特性是否可以被修改（默认为false）
 
-- enumerable： 键值为 true 时，该属性才会出现在对象的枚举属性中（循环时打印不出来）（默认为false）
+- enumerable： 定义了对象的属性是否可以在 for...in 循环和 Object.keys() 中被枚举（默认为false）
 
 - value：该属性对应的值。可以是任何有效的 JavaScript 值（数值，对象，函数等）（默认为undefined）
 
-- writable：是否可以被改变（默认false）
+- writable：当 writable 属性设置为 false 时，该属性被称为“不可写的”，它不能被重新赋值（默认false）
 
 - get：属性的getter函数，当访问该属性时，会调用此函数，执行时不传入任何参数，但是会传入 this 对象（由于继承关系，这里的this并不一定是定义该属性的对象）。该函数的返回值会被用作属性的值（默认为undefined）
 
 - set：属性的setter函数，当属性值被修改时，会调用此函数，该方法接受一个参数（也就是被赋予的新值），会传入赋值时的 this 对象（默认为undefined）
 
 
-> 如果一个描述符同时拥有 value 或 writable 和 get 或 set 键，则会产生一个异常
+> 如果一个描述符同时拥有 (value 或 writable) 和 (get 或 set ) 键，则会产生一个异常
 
-> TypeError: Invalid property descriptor. Cannot both specify accessors and a value or writable attribute
+
+```JavaScript
+let obj = {}
+
+// 在对象中添加一个存取描述符属性的示例
+let aValue = 1;
+Object.defineProperty(obj, 'a', {
+    enumerable: true,
+    configurable: true,
+    get: function () {
+        return aValue
+    },
+    set: function (val){
+        aValue = val
+    }
+})
+
+// 在对象中添加一个属性与数据描述符的示例
+Object.defineProperty(obj, 'b', {
+    value: 1,
+    writable: true,
+    enumerable: true,
+    configurable: true
+})
+
+// 数据描述符和存取描述符不能混合使用
+// 会抛出错误：TypeError: value appears only in data descriptors, get appears only in accessor descriptors
+Object.defineProperty(obj, 'c', {
+    value: 1,
+    get: function () {
+        return aValue
+    }
+})
+```
